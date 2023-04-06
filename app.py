75ad27380e12ab2fe39a9df91c5d70e926ecd55d
Emmanuel Alabi <emmanuelxxix@gmail.com>
2023-04-06 14:11:21 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def about():
 def malls_and_shopping_centers():
     return render_template("malls-and-shopping-centers.html")
 
+@app.route("/mall")
+def mall():
+    return render_template("mall.html")
+
 @app.route("/hotels")
 def hotels():
     return render_template("hotels.html")
@@ -21,6 +25,17 @@ def hotels():
 def restaurants():
     return render_template("restaurants.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin-login.html")
 
 
 
